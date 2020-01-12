4b8c02fa53d30b31f9a769760856ff566767cacf
eddiekaung <eddiekaung@github.com>
2020-01-12 08:20:59 -0800

    changed stuff

--- app/firebase/companyList.js
+++ app/firebase/companyList.js
@@ -1,6 +1,7 @@
 var db = firebase.firestore();
 
 function getListOfCompanies() {
+    var msg = "";
     db.collection("companies").get()
     .then(function(querySnapshot) {
         querySnapshot.docs.forEach(doc => {
@@ -11,7 +12,26 @@ function getListOfCompanies() {
                     let data = doc.data();
                     console.log(data["name"]);
                     name = data["name"];
-                    loadName(name);
+                    msg = `\
+                <div class="card mb-3">\
+                <div class="row no-gutters">\
+                <div class="col-md-4">\
+                <img src="..." class="card-img" alt="...">\
+                </div>\
+                <div class="col-md-8">\
+                <div class="card-body">`;
+                    msg = msg + loadName(name);
+                    msg = msg + loadBio(data["bio"]);
+                    msg = msg + `\
+                    </div>\
+                    </div>\
+                    </div>\
+                    </div>`;
+                var list = document.getElementsByClassName("company-list");
+                console.log(list);
+                list[0].innerHTML = list[0].innerHTML.replace('', msg);
+                console.log("sadfsa");
+                console.log(list);
                 }
                 else {
                     console.log("No such document!");
@@ -22,17 +42,18 @@ function getListOfCompanies() {
 }
 
 function loadName(name) {
-    var div = document.getElementById('company_name');
-    console.log(`name is ${name}`);
+    return `<h5 class="card-title" id="company_name">${name}</h5>`;
 
     // replace text in HTML string:
-    div.innerHTML = div.innerHTML.replace('', name);
-
     // manipulating text node:
-    for(var node of div.childNodes){
-        if(node.nodeType == 3 && node.textContent == '')
-            node.textContent = name;
-    }
+    //for(var node of div.childNodes){
+    //    if(node.nodeType == 3 && node.textContent == '')
+    //        node.textContent = name;
+    //}
+}
+
+function loadBio(bio) {
+    return `<p class="card-text">${bio}</p>`;
 }
 
 getListOfCompanies();
