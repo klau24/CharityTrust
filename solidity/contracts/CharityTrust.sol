ab253103c13a5c9627a1ac5a8758031aedc029af
Sullivan <sxiong5307@gmail.com>
2020-01-12 01:53:48 -0800

    blockchain finished

--- solidity/contracts/CharityTrust.sol
+++ solidity/contracts/CharityTrust.sol
@@ -1,72 +1,97 @@
 pragma solidity >=0.4.21 <0.7.0;
-
 contract CharityTrust {
     struct Charity {
         string name;
         uint256 numGoals;
         uint256 currGoal;
         uint256 numOfDonors;
-        mapping(uint256=>address) donors;
-        mapping(address=>uint256) withheldFunds;
-        mapping(uint256=>uint256) goalCount;
-        mapping(uint256=>uint256) goalValue;
-        mapping(address=>bool) voted;
+        uint256 yesVotes;
+        uint256 totalVotes;
         address charityAddress;
+        mapping(uint256=>address payable) donors;
+        mapping(address=>uint256) withheldFunds;
+        mapping(address=>bool) voted;
     }
 
+    mapping(address=>Charity) public charities;
 
-    mapping(address=>Charity) charities;
+    function createCharity(string memory _name, address payable _charityAddress) public {
+        charities[_charityAddress] = Charity(_name, 3, 1, 0, 0, 0, _charityAddress);
+    }
 
-    function deposit(address payee) public payable {
+    function getName(address payable _charityAddress) public view returns (string memory) {
+        Charity memory charity = charities[_charityAddress];
+        return charity.name;
+    }
+
+    function donate(address payable _donorAddress, address payable _charityAddress) public payable {
         uint256 amount = msg.value;
-        deposits[payee] = deposits[payee] + amount;
+        Charity storage charity = charities[_charityAddress];
+        charity.numOfDonors++;
+        charity.donors[charity.numOfDonors] = _donorAddress;
+        charity.withheldFunds[_donorAddress] = charity.withheldFunds[_donorAddress] + amount;
     }
 
-    function withdraw(address payable payee) public {
-        uint256 payment = deposits[payee];
-        deposits[payee] = 0;
-        payee.transfer(payment);
-    }
-
-    function createCharity(string _name, address _charityAddress) public {
-        charities[_charityAddress] = Charity(_name, 3, 1, 0, 0, 0, 0, 0, 0, _charityAddress);
-        charity = charities[_charityAddress];
-        charity.goalValue[1] = 2;
-        charity.goalValue[2] = 5;
-        charity.goalValue[3] = 10;
-    }
-
-    function donate() public {
-
-    }
-
-    function vote(address _voterAddress, address _charityAddress) public {
-        charity = charities[_charityAddress];
+    function vote(address _voterAddress, address payable _charityAddress, bool yes) public {
+        Charity storage charity = charities[_charityAddress];
+        require(!charity.voted[_voterAddress], "Person voted already");
+        charity.voted[_voterAddress] = true;
+        charity.totalVotes ++;
+        require(yes, "This person voted no");
+        charity.yesVotes ++;
 
         // pseudo code
-        if (vote == 2)
-            calculateTotalFunds(_charityAddress)
+        if (charity.totalVotes >= 4 && calculateThreshold(charity.yesVotes, charity.totalVotes)) {
+            calculateTotalFunds(_charityAddress);
+        }
+        else if (charity.totalVotes == 10) {
+            paybackDonors(charity);
+        }
     }
 
-    function calculateTotalFunds(address _charityAddress) private {
+    function calculateThreshold(uint256 yes, uint256 total) private pure returns (bool){
+        return ((yes * 100000) / total) >= 70000;
+    }
+
+    function calculateTotalFunds(address payable _charityAddress) private {
         //loop through all donor addresss
-        totalfunds
-        payCharity(_charityAddress, totalFunds)
+        Charity storage charity = charities[_charityAddress];
+        uint256 totalFunds;
+        uint256 i;
+
+        for (i = 1; i <= charity.numOfDonors; i++) {
+            address donor = charity.donors[i];
+            uint256 amount = charity.withheldFunds[donor] / 3;
+            charity.withheldFunds[donor] = charity.withheldFunds[donor] - amount;
+            totalFunds = totalFunds + amount;
+            charity.voted[donor] = false;
+        }
+
+        payCharity(_charityAddress, totalFunds);
     }
 
     function payCharity(address payable _charityAddress, uint256 amount) private {
         //pay charity
+        Charity storage charity = charities[_charityAddress];
+
+        charity.currGoal++;
+        charity.yesVotes = 0;
+        charity.totalVotes = 0;
+
+        _charityAddress.transfer(amount);
     }
 
-    function nextGoal() private {
+    function paybackDonors(Charity storage charity) private {
+        uint256 i;
+        address payable donor;
 
-    }
-
-    function paybackDonors(Charity charity) private {
-        for (i=1;i<=charity.numDonors;i++) {
-            donor payable = charity.donors[i];
-            donorAmount = withheldFunds[donor]
+        for (i = 1; i <= charity.numOfDonors; i++) {
+            donor = charity.donors[i];
+            uint256 donorAmount = charity.withheldFunds[donor];
             donor.transfer(donorAmount);
         }
+
+        charity.yesVotes = 0;
+        charity.totalVotes = 0;
     }
 }
--- solidity/test/CharityTrust.js
+++ solidity/test/CharityTrust.js
@@ -5,7 +5,10 @@ contract("CharityTrust", function(accounts) {
     it("initialize charitytrust", function() {
         return CharityTrust.deployed().then(function(instance) {
             charityTrustInstance = instance;
-            assert.equals(charityTrustInstance.created(), true);
+            charityTrustInstance.createCharity("SullivanCo", "0xc04C4e06cb44FbD63ce74D29ffC72bce03410B0a")
+            return charityTrustInstance.getName("0xc04C4e06cb44FbD63ce74D29ffC72bce03410B0a");
+        }).then(function(i) {
+            assert.equal(i, "SullivanCo");
         })
     })
 
